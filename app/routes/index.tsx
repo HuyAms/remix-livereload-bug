39156673b9fbc1725d88bd3ba65423c7a7a116fe
Huy Trinh <dinhhuyams@gmail.com>
2022-03-31 23:06:17 +0700

    first commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Try to edit this text</h1>
+      <p>Does the hot reload work when you edit the text?</p>
+    </div>
+  );
+}
